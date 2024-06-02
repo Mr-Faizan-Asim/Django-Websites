11188bdfc86d9e63978c8b7ab7b6424949870720
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-06-02 21:52:34 +0500

    2nd Work View All Added

--- employee_management_system/emp_app/urls.py
+++ employee_management_system/emp_app/urls.py
@@ -19,9 +19,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index , name="index")
+    path('',views.index , name="index"),
     path('all_employee',views.all , name="all")
-    path('add',views.add , name="add")
-    path('remove',views.remove , name="remove")
-    path('filter',views.filter , name="filter")
+    #path('add',views.add , name="add"),
+    #path('remove',views.remove , name="remove"),
+    #path('filter',views.filter , name="filter")
 ]
--- employee_management_system/emp_app/views.py
+++ employee_management_system/emp_app/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
+
+def all(request):
+    
+    return render(request,'all.html')
+
